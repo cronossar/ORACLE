55798735156bbf1183a318d127d12040569f5ce1
Daniel Buiak Branchesi <127250505+cronossar@users.noreply.github.com>
2024-09-13 20:08:07 -0300

    xxx

---
+++ BACKEND/JAVA/2032-java-aplicando-la-orientacion-a-objetos-aula-01/src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -0,0 +1,73 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Titulo {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDelasEvaluaciones;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public double getSumaDeLasEvaluaciones() {
+        return sumaDeLasEvaluaciones;
+    }
+
+    public void setSumaDeLasEvaluaciones(double sumaDeLasEvaluaciones) {
+        this.sumaDeLasEvaluaciones = sumaDeLasEvaluaciones;
+    }
+
+    public void setTotalDelasEvaluaciones(int totalDelasEvaluaciones) {
+        this.totalDelasEvaluaciones = totalDelasEvaluaciones;
+    }
+
+    public int getTotalDelasEvaluaciones(){
+        return totalDelasEvaluaciones;
+    }
+
+    public void muestraFichaTecnica(){
+        System.out.println("El nombre de la película es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duración en minutos: "+ getDuracionEnMinutos());
+    }
+
+    public void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDelasEvaluaciones++;
+    }
+
+    public double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalDelasEvaluaciones;
+    }
+}
