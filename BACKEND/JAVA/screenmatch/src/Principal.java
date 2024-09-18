87dd975a99a8e942cedaf7354d1f31ceee53f752
dabb <127250505+cronossar@users.noreply.github.com>
2024-09-18 11:43:46 -0300

    zzz

--- BACKEND/JAVA/screenmatch/src/Principal.java
+++ BACKEND/JAVA/screenmatch/src/Principal.java
@@ -1,34 +1,44 @@
+import com.aluracursos.screenmacth.calculos.CalculadoraDeTiempo;
+import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-        System.out.println("SCREENMATCH - Todas tus peliculas en un solo lugar");
-        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Encanto";
-        miPelicula.fechaDeLanzamiento = 2019;
-        miPelicula.duracionEnMinutos = 120;
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
         miPelicula.muestraFichaTecnica();
-        miPelicula.evalua(7.8);
         miPelicula.evalua(10);
-        System.out.println("La sumatoria de puntos de las evaluaciones para " + miPelicula.nombre + " es: " + miPelicula.sumaDeLasEvaluaciones);
-        System.out.println("Evaluaciones realizadas: " + miPelicula.totalDeLasEvaluaciones);
-        System.out.println("La media calculada de las evaluaciones es: " + miPelicula.calculaMedia());
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+        System.out.println(miPelicula.getTotalDelasEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La casa del dragón");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.setNombre("Matrix");
+        otraPelicula.setFechaDeLanzamiento(1998);
+        otraPelicula.setDuracionEnMinutos(180);
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(casaDragon);
+        calculadora.incluye(otraPelicula);
+        System.out.println("Tiempo necesario para ver tus títulos favoritos estas vacaciones "
+                + calculadora.getTiempoTotal() + " minutos");
 
 
-        System.out.println("===================");
-
-        Pelicula miPelicula2 = new Pelicula();
-        miPelicula2.nombre = "Matrix";
-        miPelicula2.fechaDeLanzamiento = 1998;
-        miPelicula2.duracionEnMinutos = 130;
-        miPelicula2.muestraFichaTecnica();
-        miPelicula2.evalua(7.9);
-        miPelicula2.evalua(8.6);
-        miPelicula2.evalua(10);
-        miPelicula2.evalua(9.5);
-        System.out.println("La sumatoria de puntos de las evaluaciones para " + miPelicula2.nombre + " es: " + miPelicula2.sumaDeLasEvaluaciones);
-        System.out.println("Evaluaciones realizadas: " + miPelicula2.totalDeLasEvaluaciones);
-        System.out.println("La media calculada de las evaluaciones es: " + miPelicula2.calculaMedia());
 
 
 
