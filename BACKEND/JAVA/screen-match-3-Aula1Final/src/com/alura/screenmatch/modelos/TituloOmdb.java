b8569796ab1dc98bc2c792b8cbfe48c3a361717f
Daniel Buiak Branchesi <127250505+cronossar@users.noreply.github.com>
2024-09-26 20:22:27 -0300

    xxxx

---
+++ BACKEND/JAVA/screen-match-3-Aula1Final/src/com/alura/screenmatch/modelos/TituloOmdb.java
@@ -0,0 +1,6 @@
+package com.alura.screenmatch.modelos;
+
+public record TituloOmdb(String title, String year, String runtime) {
+
+
+}
--- BACKEND/JAVA/screen-match-3-Aula1Final/src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
+++ BACKEND/JAVA/screen-match-3-Aula1Final/src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
@@ -1,5 +1,11 @@
 package com.alura.screenmatch.principal;
 
+import com.alura.screenmatch.modelos.Titulo;
+import com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -7,13 +13,16 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
+
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner lectura = new Scanner(System.in);
         System.out.println("Escriba el nombre de una pelicula a buscar: ");
         var busqueda = lectura.nextLine();
 
-        String direccion = "http://www.omdbapi.com/?t="+busqueda+"&apikey=fa0fa535";
+        String direccion = "http://www.omdbapi.com/?t="+
+                busqueda.replace(" ", "+")
+                +"&apikey=fa0fa535";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -23,7 +32,25 @@ public class PrincipalConBusqueda {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+        System.out.println("xxxxxxxxxxxxx");
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy
+                .UPPER_CAMEL_CASE).create();
+
+        TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+
+        System.out.println(miTituloOmdb);
+        System.out.println("xxxxxxxxxxxxx");
+        try {
+            Titulo miTitulo = new Titulo(miTituloOmdb);
+            System.out.println("Título ya convertido: " + miTituloOmdb);
+        } catch (NumberFormatException e){
+            System.out.println("Ocurrio un error: ");
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Finalizo la ejecución del programa!");
 
     }
 }
