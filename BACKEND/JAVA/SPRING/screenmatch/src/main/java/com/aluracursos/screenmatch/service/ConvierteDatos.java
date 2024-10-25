5f9e9c1b90ec51be9c6c478c58344c6e47b4bdc3
dabb <127250505+cronossar@users.noreply.github.com>
2024-10-25 10:22:40 -0300

    xxx

--- BACKEND/JAVA/SPRING/screenmatch/src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
+++ BACKEND/JAVA/SPRING/screenmatch/src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
@@ -1,9 +1,22 @@
 package com.aluracursos.screenmatch.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.json.JsonParseException;
 
-
-public class ConvierteDatos {
+public class ConvierteDatos implements IConvierteDatos {
 
     private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+
+        try {
+            return objectMapper.readValue(json,clase);
+        } catch (JsonParseException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
